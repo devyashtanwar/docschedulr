b9002054ea700b9c76d93ae6e9af837da846b597
devyash_tanwar <tanwardevyash@gmail.com>
2024-11-10 23:08:42 +0530

    Changes to the backend

--- server/index.js
+++ server/index.js
@@ -17,11 +17,6 @@ app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(port, () => {});
 console.log(`Server running at ${port}`);
